10d398773388b4bfc09edec5c2ef8ce869be9ab5
Muhammad Adib <adbahnf19@gmail.com>
2026-05-25 08:38:57 +0800

    Update icon

--- vgm-stock-take/vite.config.ts
+++ vgm-stock-take/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'app-icon.svg'],
       manifest: {
         name: 'VGM CKD',
         short_name: 'VGM CKD',
